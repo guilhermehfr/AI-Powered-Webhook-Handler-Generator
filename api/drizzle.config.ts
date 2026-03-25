18a11b7cef7008731337c4be3bc1cd61d31a95ea
guilhermehfr <guihenrique.bra@gmail.com>
2026-03-25 18:49:38 -0400

    feat: create api structure with docker, drizzle and fastify

---
+++ api/drizzle.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from "drizzle-kit";
+import { env } from "@/env";
+
+export default defineConfig({
+	dialect: "postgresql",
+	dbCredentials: { url: env.DATABASE_URL },
+	out: "./src/db/migrations",
+	schema: "./src/db/schema/index.ts",
+	casing: "snake_case",
+});
--- api/src/server.ts
+++ api/src/server.ts
@@ -1,10 +1,46 @@
-import {fastify} from 'fastify';
+import { fastify } from "fastify";
+import {
+	serializerCompiler,
+	validatorCompiler,
+	jsonSchemaTransform,
+	type ZodTypeProvider,
+} from "fastify-type-provider-zod";
+import { fastifyCors } from "@fastify/cors";
+import { fastifySwagger } from "@fastify/swagger";
+import scalarApiReference from "@scalar/fastify-api-reference";
 
-const app = fastify();
+import { listWebhooks } from "./routes/list-webhooks";
 
-app.listen({port: 3333, host: '0.0.0.0'}).then(() => {
-  console.log('HTTP Server is running on port 3333');
-}).catch((err) => {
-  console.error('Error starting server:', err);
-  process.exit(1);
+import { env } from "./env";
+
+const app = fastify().withTypeProvider<ZodTypeProvider>();
+app.setValidatorCompiler(validatorCompiler);
+app.setSerializerCompiler(serializerCompiler);
+
+app.register(fastifyCors, {
+	origin: true,
+	methods: ["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
+	// credentials: true,
+});
+
+app.register(fastifySwagger, {
+	openapi: {
+		info: {
+			title: "Webhook Inspector API",
+			description: "API for capturing and inspecting webhook requests",
+			version: "1.0.0",
+		},
+	},
+	transform: jsonSchemaTransform,
+});
+
+app.register(listWebhooks);
+
+app.register(scalarApiReference, {
+	routePrefix: "/docs",
+});
+
+app.listen({ port: env.PORT }).then(() => {
+	console.log("- HTTP Server running on http://localhost:3333");
+	console.log("- Docs available at http://localhost:3333/docs");
 });
