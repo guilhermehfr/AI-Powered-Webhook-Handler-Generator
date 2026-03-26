9b34869c36658168c67f6b67391912989ec01857
guilhermehfr <guihenrique.bra@gmail.com>
2026-03-25 22:11:28 -0400

    refactor: organize and consolidate changes

---
+++ web/src/components/ui/Sidebar.tsx
@@ -0,0 +1,12 @@
+export function Sidebar() {
+  return (
+    <div className="flex h-screen flex-col">
+      <div className="flex items-center justify-between border-b border-zinc-700 px-4 py-5">
+        <div className="flex items-baseline">
+          <span className="font-semibold text-zinc-100">weebhook</span>
+          <span className="font-normal text-zinc-400">.inpect</span>
+        </div>
+      </div>
+    </div>
+  );
+}
