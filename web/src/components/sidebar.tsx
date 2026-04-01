9d2f32ed65f2a8da2f3160b902dfa0bec4982292
guilhermehfr <guihenrique.bra@gmail.com>
2026-03-31 21:29:40 -0400

    feat: update title in index.html and modify sidebar component to reflect new branding

--- web/src/components/sidebar.tsx
+++ web/src/components/sidebar.tsx
@@ -3,14 +3,14 @@ import { WebhooksList } from './webhooks-list';
 import { Suspense } from 'react';
 
 export function Sidebar() {
-  const webhooksPathname = 'https://webhook-handler-generator-api.vercel.app/api/webhooks';
+  const webhooksPathname = 'https://ai-powered-webhook-handler-generator.onrender.com/api/webhooks';
 
   return (
     <div className="flex h-screen flex-col">
       <div className="flex items-center justify-between border-b border-zinc-700 px-4 py-5">
         <div className="flex items-baseline">
           <span className="font-semibold text-zinc-100">webhook</span>
-          <span className="font-normal text-zinc-400">.inspect</span>
+          <span className="font-normal text-zinc-400">.ai-powered</span>
         </div>
       </div>
 
